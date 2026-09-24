8c794b76b53b7193764ef649afbff11d05779e77
fahimasaniya8 <fahimasaniya8@gmail.com>
2026-09-24 22:31:25 +0600

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,7 +27,6 @@ export default function RootLayout({ children }) {
       <body className="min-h-full flex flex-col">
         <div>{children}</div>
         <Navbar></Navbar>
-        <Banner></Banner>
       </body>
     </html>
   );
